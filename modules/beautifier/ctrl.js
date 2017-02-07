33284335c62b47161e260fffc321a9950813a67a
Ravi Nain <cdacravinain@gmail.com>
2017-02-07 21:15:31 +1000

    Added tabs feature

--- modules/beautifier/ctrl.js
+++ modules/beautifier/ctrl.js
@@ -2,20 +2,22 @@
   var myApp = angular.module('beautifierApp');
   
   myApp.controller('beautifierCtrl', ['$scope', 'beautifierService', function($scope, beautifierService){
-    $scope.inputJson = "";
+    $scope.inputJson = [''];
     $scope.outputJson = {};
     $scope.showChildren = true;
+    $scope.tabs = [{tabName:'Tab 1', active: true}];
+    $scope.activeTab = 0;
 
     $scope.isValidInput = function(){
-        return beautifierService.isValidInput($scope.inputJson)
+        return beautifierService.isValidInput($scope.inputJson[$scope.activeTab]);
     };
 
     $scope.updateOutput = function(){
-        $scope.outputJson = beautifierService.getParsedOutput($scope.inputJson);
+        $scope.outputJson = beautifierService.getParsedOutput($scope.inputJson[$scope.activeTab]);
     };
 
     $scope.isEmptyInput = function(){
-        return beautifierService.isEmpty($scope.inputJson)
+        return beautifierService.isEmpty($scope.inputJson[$scope.activeTab]);
     };
       
     $scope.isArrayType = function(obj){
@@ -24,6 +26,26 @@
 
     $scope.isObjectType = function(obj){
         return beautifierService.isObjectType(obj);
+    };
+      
+    $scope.toggleTab = function(activeIndex) {
+      var length = $scope.tabs.length;
+        
+      for(var c=0; c<length; c++) {
+          if(activeIndex == c) {
+              $scope.tabs[c].active = true;
+              $scope.activeTab = activeIndex;
+              $scope.updateOutput();
+          } else {
+              $scope.tabs[c].active = false;
+          }
+      }
+    };
+      
+    $scope.addTab = function(){
+      $scope.tabs.push({tabName: 'Tab '+($scope.tabs.length+1), active:false});
+      $scope.inputJson.push('');
+      $scope.toggleTab($scope.tabs.length-1);
     };
   }]);
   
